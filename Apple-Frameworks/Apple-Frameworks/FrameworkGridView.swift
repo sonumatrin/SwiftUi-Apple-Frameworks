4a6eb8ab37c57e627553fed27ab1c6ee96175edb
sonumatrin <sonumartin007@gmail.com>
2024-05-02 16:06:41 -0400

    Initial Push

---
+++ Apple-Frameworks/Apple-Frameworks/FrameworkGridView.swift
@@ -0,0 +1,20 @@
+//
+//  FrameworkGridView.swift
+//  Apple-Frameworks
+//
+//  Created by Sonu Martin on 2024-05-02.
+//
+
+import SwiftUI
+
+struct FrameworkGridView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FrameworkGridView_Previews: PreviewProvider {
+    static var previews: some View {
+        FrameworkGridView()
+    }
+}
